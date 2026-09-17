13dfd0d2d21a22cd71d22409496efa28a9ac8f4a
Mitra Bhuvanendran <mitrabhuvanendran@gmail.com>
2026-09-17 12:22:36 +0530

    Place IoT Session navigation link before Contact

--- iot-nav.js
+++ iot-nav.js
@@ -6,7 +6,9 @@
     link.href = '/iot-session.html';
     link.textContent = 'IoT Session';
     link.dataset.iotLink = 'true';
-    nav.appendChild(link);
+    const contact = [...nav.querySelectorAll('a')].find(item => item.getAttribute('href') === '/contact');
+    if (contact) nav.insertBefore(link, contact);
+    else nav.appendChild(link);
   };
   addIoTLink();
   new MutationObserver(addIoTLink).observe(document.body, { childList: true, subtree: true });
